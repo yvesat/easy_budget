b0d87413012c1f0baca855968188d17b3190e056
Yves Atsuchi Takaoka <33436826+yvesat@users.noreply.github.com>
2024-03-13 15:58:54 -0300

    Clean up and minor fixes

--- lib/src/controller/auth_controller.dart
+++ lib/src/controller/auth_controller.dart
@@ -4,20 +4,21 @@ import 'package:go_router/go_router.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../model/custom_exception.dart';
+import '../model/enums/alert_type.dart';
 import '../model/isar_service.dart';
 import '../model/services/user_services.dart';
 import '../model/user_model.dart';
+import '../view/widgets/alert.dart';
 
 class AuthController extends StateNotifier<AsyncValue<void>> {
   AuthController() : super(const AsyncValue.data(null));
 
-  // final UserRepository repositorioUsuario = UserRepository();
-  final IsarService isarService = IsarService();
+  final IsarService _isarService = IsarService();
 
   Future<UserModel?> loadUser() async {
     try {
       state = const AsyncValue.loading();
-      return await isarService.getUserDB();
+      return await _isarService.getUserDB();
     } catch (_) {
       rethrow;
     } finally {
@@ -33,7 +34,7 @@ class AuthController extends StateNotifier<AsyncValue<void>> {
 
       state = const AsyncValue.loading();
 
-      final userCredential = await UserServices.logIn(context, email, password);
+      await UserServices.logIn(context, email, password);
 
       if (context.mounted) context.go('/home');
     } catch (e) {
@@ -53,7 +54,7 @@ class AuthController extends StateNotifier<AsyncValue<void>> {
 
       await UserServices.signUp(context, email, password);
 
-      if (context.mounted) context.go('/home');
+      if (context.mounted) Alert.dialog(context, AlertType.sucess, AppLocalizations.of(context)!.successUserCreation);
     } catch (e) {
       rethrow;
     } finally {
